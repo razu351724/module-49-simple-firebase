07503e93ced0ccbf77d45393f262451d8335fd67
RAZU-sheikh <razu351724@gmail.com>
2023-09-29 19:16:07 +0600

    <feat>: react route and firebase satap in web site

---
+++ src/firebase/firebase.inti.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvv-4NnJGuQ1GfvR8leXML47g6RgYvuhg",
+  authDomain: "simple-firebase-65f41.firebaseapp.com",
+  projectId: "simple-firebase-65f41",
+  storageBucket: "simple-firebase-65f41.appspot.com",
+  messagingSenderId: "470102134639",
+  appId: "1:470102134639:web:dc55987d32c7c186a8229a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
